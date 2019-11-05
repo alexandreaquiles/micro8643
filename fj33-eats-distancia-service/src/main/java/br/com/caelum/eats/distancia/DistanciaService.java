aed517395c28fe0fff2c6206ddb5f873b3fe22a0
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-11-05 16:22:59 -0300

    capítulo 3 - extrai serviço de distância

--- fj33-eats-distancia-service/src/main/java/br/com/caelum/eats/distancia/DistanciaService.java
+++ fj33-eats-distancia-service/src/main/java/br/com/caelum/eats/distancia/DistanciaService.java
@@ -8,9 +8,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import br.com.caelum.eats.administrativo.TipoDeCozinha;
-import br.com.caelum.eats.restaurante.Restaurante;
-import br.com.caelum.eats.restaurante.RestauranteService;
 import lombok.AllArgsConstructor;
 
 /*
@@ -22,9 +19,9 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 class DistanciaService {
 
-	private static final Pageable LIMIT = PageRequest.of(0,5);
+	private static final Pageable LIMIT = PageRequest.of(0, 5);
 
-	private RestauranteService restaurantes;
+	private RestauranteRepository restaurantes;
 
 	List<RestauranteComDistanciaDto> restaurantesMaisProximosAoCep(String cep) {
 		List<Restaurante> aprovados = restaurantes.findAllByAprovado(true, LIMIT).getContent();
@@ -32,29 +29,27 @@ class DistanciaService {
 	}
 
 	List<RestauranteComDistanciaDto> restaurantesDoTipoDeCozinhaMaisProximosAoCep(Long tipoDeCozinhaId, String cep) {
-		TipoDeCozinha tipo = new TipoDeCozinha();
-		tipo.setId(tipoDeCozinhaId);
-		List<Restaurante> aprovadosDoTipoDeCozinha = restaurantes.findAllByAprovadoAndTipoDeCozinha(true, tipo, LIMIT).getContent();
+		List<Restaurante> aprovadosDoTipoDeCozinha = restaurantes
+				.findAllByAprovadoAndTipoDeCozinhaId(true, tipoDeCozinhaId, LIMIT).getContent();
 		return calculaDistanciaParaOsRestaurantes(aprovadosDoTipoDeCozinha, cep);
 	}
 
 	RestauranteComDistanciaDto restauranteComDistanciaDoCep(Long restauranteId, String cep) {
-		Restaurante restaurante = restaurantes.findById(restauranteId).orElseThrow(() -> new ResourceNotFoundException());
+		Restaurante restaurante = restaurantes.findById(restauranteId)
+				.orElseThrow(() -> new ResourceNotFoundException());
 		String cepDoRestaurante = restaurante.getCep();
 		BigDecimal distancia = distanciaDoCep(cepDoRestaurante, cep);
 		return new RestauranteComDistanciaDto(restauranteId, distancia);
 	}
 
-	private List<RestauranteComDistanciaDto> calculaDistanciaParaOsRestaurantes(List<Restaurante> restaurantes, String cep) {
-		return restaurantes
-				.stream()
-				.map(restaurante -> {
-					String cepDoRestaurante = restaurante.getCep();
-					BigDecimal distancia = distanciaDoCep(cepDoRestaurante, cep);
-					Long restauranteId = restaurante.getId();
-					return new RestauranteComDistanciaDto(restauranteId, distancia);
-				})
-				.collect(Collectors.toList());
+	private List<RestauranteComDistanciaDto> calculaDistanciaParaOsRestaurantes(List<Restaurante> restaurantes,
+			String cep) {
+		return restaurantes.stream().map(restaurante -> {
+			String cepDoRestaurante = restaurante.getCep();
+			BigDecimal distancia = distanciaDoCep(cepDoRestaurante, cep);
+			Long restauranteId = restaurante.getId();
+			return new RestauranteComDistanciaDto(restauranteId, distancia);
+		}).collect(Collectors.toList());
 	}
 
 	private BigDecimal distanciaDoCep(String cepDoRestaurante, String cep) {
@@ -62,14 +57,14 @@ class DistanciaService {
 	}
 
 	private BigDecimal calculaDistancia() {
-		//simulaDemora();
-		return new BigDecimal(Math.random()*15);
+		// simulaDemora();
+		return new BigDecimal(Math.random() * 15);
 	}
 
 	@SuppressWarnings("unused")
 	private void simulaDemora() {
-		//simula demora de 10s a 20s
-		long demora = (long) (Math.random()*10000+10000);
+		// simula demora de 10s a 20s
+		long demora = (long) (Math.random() * 10000 + 10000);
 		try {
 			Thread.sleep(demora);
 		} catch (InterruptedException e) {
