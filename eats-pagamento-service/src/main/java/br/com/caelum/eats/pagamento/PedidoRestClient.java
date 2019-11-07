a909d8c01e8ce461f5ea7ed90a7edc10e6d4da66
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-11-07 08:47:40 -0300

    capítulo 7 - client side load balancing

--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/PedidoRestClient.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/PedidoRestClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(url="${monolito.url}", name = "monolito")
+@FeignClient("monolito")
 interface PedidoRestClient {
 
 	@PutMapping("/pedidos/{pedidoId}/pago")
