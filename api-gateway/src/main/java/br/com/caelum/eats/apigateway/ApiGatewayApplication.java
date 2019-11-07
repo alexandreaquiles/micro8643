d9f62554c6fcd50139ab158cf13bb7e5264922b7
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-11-07 11:31:08 -0300

    capítulos 8 e 9 até fallback @HystrixCommand

--- api-gateway/src/main/java/br/com/caelum/eats/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/br/com/caelum/eats/apigateway/ApiGatewayApplication.java
@@ -2,9 +2,13 @@ package br.com.caelum.eats.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableZuulProxy
 @SpringBootApplication
--- api-gateway/src/main/java/br/com/caelum/eats/apigateway/DistanciaRestClient.java
+++ api-gateway/src/main/java/br/com/caelum/eats/apigateway/DistanciaRestClient.java
@@ -1,10 +1,13 @@
 package br.com.caelum.eats.apigateway;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 class DistanciaRestClient {
@@ -19,9 +22,13 @@ class DistanciaRestClient {
 		this.distanciaServiceUrl = distanciaServiceUrl;
 	}
 
+	@HystrixCommand(fallbackMethod = "restauranteSemDistancia")
 	Map<String, Object> porCepEId(String cep, Long restauranteId) {
 		String url = distanciaServiceUrl + "/restaurantes/" + cep + "/restaurante/"+ restauranteId;
 		return restTemplate.getForObject(url, Map.class);
 	}
 	
+	Map<String, Object> restauranteSemDistancia(String cep, Long restauranteId) {
+		return new HashMap<>();
+	}
 }
